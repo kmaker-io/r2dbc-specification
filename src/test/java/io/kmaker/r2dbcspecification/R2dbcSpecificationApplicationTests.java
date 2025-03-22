4d0c06557995ebb7402589024f0b3332ed72380b
TOUCH Yong An <yongan.touch@kmaker.io>
2025-03-22 14:26:03 +0700

    init project

---
+++ src/test/java/io/kmaker/r2dbcspecification/R2dbcSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kmaker.r2dbcspecification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcSpecificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
